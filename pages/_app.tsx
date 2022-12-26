ab6281711ae055c1d8f015805a22b6815c0c7b0f
Dengel <52581244+DengelLionel@users.noreply.github.com>
2022-12-26 11:30:12 -0500

    corrected error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {LandingHandler} from "../context/landing";
+import {LandingHandler} from "../context/Landing";
 export default function App({ Component, pageProps }: AppProps) {
   return <LandingHandler>
     <Component {...pageProps} />
