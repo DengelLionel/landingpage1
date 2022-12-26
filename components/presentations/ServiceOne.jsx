eb070a5b08214e030b02f1b3c9f367d0a416b1f1
Dengel <52581244+DengelLionel@users.noreply.github.com>
2022-12-26 11:34:33 -0500

    improvement interactividad in serviceOne

--- components/presentations/ServiceOne.jsx
+++ components/presentations/ServiceOne.jsx
@@ -8,7 +8,7 @@ const ServiceOne = () => {
   },[openProductoUno])
   console.log("open p",openProductoUno)
   return (
-    <section className={`fixed transition-all duration-500 p-[15px] ${openProductoUno===true?'left-0':'-left-[200%]'}`}>
+    <section className={`fixed transition-all duration-500 w-full p-[15px] ${openProductoUno===true?'left-0':'-left-[100%]'}`}>
       <h2 className='font-extrabold font-Poppins text-[29px]'>Creamos Soluciones Tecnológicas</h2>
       <p className='font-Roboto font-medium text-[20px] mt-[21px] mb-[21px]'>Seamos parte de colaborar con soluciones digitales eficientes para tu negocio.</p>
       <button className='rounded-b-full rounded-t-full bg-secundary w-full p-[8px] text-white1 font-extrabold text-[20px] font-Poppins'>Pide Ahora</button>
